0790c8821b59e4f4a1c629bfed4595815bb112e1
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-09-04 16:25:20 -0400

    more pages

--- core_app/views.py
+++ core_app/views.py
@@ -25,7 +25,11 @@ def hello(request):
 
 
 def home(request):
-  return render(request, 'home.html')
+    return render(request, 'home.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
 
 
 def signup(request):
