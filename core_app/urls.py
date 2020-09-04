95abe92522db5a67addaa6544ae5b1739dab461f
unknown <draz@ualberta.ca>
2020-09-04 10:06:22 -0600

    search.html and beautifulsoup set up

--- core_app/urls.py
+++ core_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', views.signup, name='signup')
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/login/search/', views.search, name='search')
 ]
