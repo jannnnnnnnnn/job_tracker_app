e5b0af95f259c367ca1572689bbe4469247ec92a
unknown <draz@ualberta.ca>
2020-09-07 09:14:02 -0600

    path changes

--- core_app/urls.py
+++ core_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
-    path('search/', views.search, name='search'),
+    path('profile/search/', views.search, name='search'),
     path('profile/', views.profile, name='profile'),
     path('apptracker/', views.apptracker, name='apptracker'),
     path('profile_form', views.ProfileCreate.as_view(), name='profile_create'),
