4c8198e9872d5a896146d17d9717956b9e83ad8d
joecotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-09-07 16:17:59 -0400

    html changes

--- job_tracker_app/settings.py
+++ job_tracker_app/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/profile/'
+LOGIN_REDIRECT_URL = '/search/'
 LOGOUT_REDIRECT_URL = '/'
 
 CITIES_LIGHT_TRANSLATION_LANGUAGES = ['en']
